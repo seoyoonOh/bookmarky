fc5939f04901d3530d36ac8c8b0672cbfe43498b
seoyoon <5happy1004kr@naver.com>
2024-04-01 01:08:28 +0900

    ✨ Actions

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -1,15 +1,15 @@
 'use server';
 
-import { revalidatePath } from '../../../node_modules/next/cache';
-import urlMetadata from '../../../node_modules/url-metadata/index';
+import { revalidatePath } from 'next/cache';
+import urlMetadata from 'url-metadata/index';
 import { Link, Tag } from './models';
 import { connectToDB } from './utils';
 
 export const addTag = async (formData) => {
+  const { name, color } = Object.fromEntries(formData);
+
   try {
     connectToDB();
-
-    const { name, color } = Object.fromEntries(formData);
     const newTag = new Tag({
       name,
       color,
@@ -23,21 +23,61 @@ export const addTag = async (formData) => {
   revalidatePath('/dashboard');
 };
 
-export const addLink = async (formData) => {
+export const updateTag = async (formData) => {
+  const { _id, ...updateFields } = Object.fromEntries(formData);
+
   try {
     connectToDB();
+    await Tag.findByIdAndUpdate(_id, updateFields);
+  } catch (error) {
+    console.log(error);
+    throw new Error('Faild to update tag!');
+  }
+};
 
-    const { url } = Object.fromEntries(formData);
-    const metadata = await urlMetadata(url);
+export const deleteTag = async (formData) => {
+  const { _id } = Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+    await Tag.findByIdAndDelete(_id);
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to delete tag!');
+  }
+
+  revalidatePath('/dashboard');
+};
+
+export const addLink = async (formData) => {
+  const { url } = Object.fromEntries(formData);
+  const metadata = await urlMetadata(url);
+
+  try {
+    connectToDB();
     const newLink = new Link({
       url,
       title: metadata.title,
-      image: '',
+      image: metadata['og:image'] || '',
     });
     await newLink.save();
   } catch (error) {
     console.log(error);
     throw new Error('Failed to create link!');
+  }
+
+  revalidatePath('/dashboard');
+};
+
+export const deleteLink = async (formData) => {
+  const { _id } = Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+    await Link.findByIdAndDelete(_id);
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to delete link!');
   }
 
   revalidatePath('/dashboard');
