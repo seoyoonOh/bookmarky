6ac0bea73e73a7d7a11e2d2794f3b03764d96315
seoyoon <5happy1004kr@naver.com>
2024-03-31 22:52:32 +0900

    💄 Link Delete Button & URL

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import styles from '../ui/dashboard/dashboard.module.css';
 import { fetchLinks, fetchLinksByTag, fetchTags } from '../lib/data';
 import Tags from '../ui/dashboard/tags/tags';
+import Link from '../../../node_modules/next/link';
+import { deleteLink } from '../lib/actions';
 
 export default async function Dashboard({ searchParams }) {
   let tags = await fetchTags();
@@ -21,11 +23,19 @@ export default async function Dashboard({ searchParams }) {
       </div>
       <div className={styles.grid}>
         {links.map(({ _id, url, title, image, tags }) => (
-          <div className={styles.link}>
-            <div className={styles.preview}>
-              <img src={image || 'https://images.pexels.com/photos/2574173/pexels-photo-2574173.jpeg?auto=compress&cs=tinysrgb&w=400'} alt="" />
-            </div>
-            <h1>{title}</h1>
+          <div className={styles.link} key={_id}>
+            <form action={deleteLink} className={styles.form}>
+              <input type="hidden" name="_id" value={_id} />
+              <button className={styles.delete}>✕</button>
+            </form>
+            <Link href={url} target="_blank">
+              <div className={styles.preview}>
+                <img src={image || 'https://images.pexels.com/photos/2574173/pexels-photo-2574173.jpeg?auto=compress&cs=tinysrgb&w=400'} alt="" />
+              </div>
+            </Link>
+            <Link href={url} target="_blank">
+              <h1>{title}</h1>
+            </Link>
           </div>
         ))}
       </div>
