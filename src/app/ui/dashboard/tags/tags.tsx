e508516d4b2c0d786b070ff6457498cc8e55c28d
seoyoon <5happy1004kr@naver.com>
2024-04-03 03:19:35 +0900

    ✨Add and Delete Tags

--- src/app/ui/dashboard/tags/tags.tsx
+++ src/app/ui/dashboard/tags/tags.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react';
 import { usePathname, useRouter, useSearchParams } from '../../../../../node_modules/next/navigation';
 import styles from './tags.module.css';
+import { addTag, deleteTag } from '@/app/lib/actions';
+import colors from '@/app/assets/colors.json';
 
 export default function Tags({ tags }: { tags: { _id: string; name: string; color: string }[] }) {
   const searchParams = useSearchParams();
@@ -10,6 +12,9 @@ export default function Tags({ tags }: { tags: { _id: string; name: string; colo
   const pathname = usePathname();
   const [selectedTag, setSelectedTag] = useState<string>('');
   const params = new URLSearchParams(searchParams);
+  const [editing, setEditing] = useState(false);
+  const [selectedColor, setSelectedColor] = useState('red');
+  const [selectingColor, setSelectingColor] = useState(false);
 
   const handleTagSelect = (event: React.MouseEvent<HTMLLIElement>) => {
     const target = event.target as HTMLLIElement;
@@ -30,10 +35,15 @@ export default function Tags({ tags }: { tags: { _id: string; name: string; colo
     replace(`${pathname}`);
   }
 
+  const handleDelete = async (tagId: string) => {
+    await deleteTag({ _id: tagId });
+  };
+
   return (
-    <ul className={styles.tags}>
+    <div className={`${styles.tags} ${editing && styles.editing}`}>
+      <div onClick={() => setEditing(false)} className={styles.overlay}></div>
       {tags.map(({ _id, name, color }: { _id: string; name: string; color: string }) => (
-        <li
+        <div
           className={`${styles.tag} ${selectedTag === '' ? '' : selectedTag !== _id ? styles.not_selected : ''}`}
           style={{ '--color': `var(--tag-${color})` }}
           key={_id}
@@ -41,8 +51,37 @@ export default function Tags({ tags }: { tags: { _id: string; name: string; colo
           onClick={handleTagSelect}
         >
           {name}
-        </li>
+          {editing && (
+            <div className={styles.delete} onClick={() => handleDelete(_id)}>
+              ✕
+            </div>
+          )}
+        </div>
       ))}
-    </ul>
+      <div className={styles.formContainer}>
+        <button className={styles.edit} onClick={() => setEditing(!editing)}>
+          +
+        </button>
+        {editing && (
+          <form action={addTag} className={styles.form} onSubmit={() => setEditing(false)}>
+            <input type="hidden" name="color" value={selectedColor} />
+            <div className={styles.color} style={{ '--color': `var(--tag-${selectedColor})` }} onClick={() => setSelectingColor(!selectingColor)}>
+              {selectingColor && (
+                <>
+                  <div onClick={() => setSelectingColor(false)} className={styles.palette_overlay}></div>
+                  <div className={styles.palette}>
+                    {colors.map(({ color }) => (
+                      <div className={styles.paletteColor} style={{ '--color': `var(--tag-${color})` }} onClick={() => setSelectedColor(color)}></div>
+                    ))}
+                  </div>
+                </>
+              )}
+            </div>
+            <input className={styles.name} type="text" placeholder="이름" name="name" />
+            <button className={styles.add}>+</button>
+          </form>
+        )}
+      </div>
+    </div>
   );
 }
